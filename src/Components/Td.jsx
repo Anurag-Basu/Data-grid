70e1a0c90b57a61da6e25697bca4cebb2d548f9d
Anurag-Basu <basugupta312@gmail.com>
2022-04-01 21:59:14 +0530

    removed one more warning

--- src/Components/Td.jsx
+++ src/Components/Td.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Td = ({ children, className }) => {
     return (
         <td
-          scope="col"
+          
           className={`text-sm  text-gray-900 px-6 py-4 text-left ${className}`}
         >
           {children}
